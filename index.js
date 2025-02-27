0a6670225fb7cd8186b5f0592e0999166df40ae7
keithkipkoech <keithselim@gmail.com>
2025-02-28 01:54:55 +0300

    acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {  
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    const gameWidth = 400;  // Game field's width is 400px
+    if (left < gameWidth - 40) {  // Prevent moving out of bounds (dodger is 40px wide)
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
